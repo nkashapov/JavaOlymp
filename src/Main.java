1f70f28f2c8b0d311250c4ff25adfdfe0ba79996
Niyaz <n.kashapov.inno@gmail.com>
2017-10-08 13:58:27 +0300

    new project

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int w,h,n,x,y;
+	    w = in.nextInt();
+        h = in.nextInt();
+        n = in.nextInt();
+        double q, Sr, Sq;
+        double left = 0, right = w*n;
+        q=(left+right)/2;
+        while(!((q/w==0)||(q/h==0))){
+            
+        }
+    }
+}
